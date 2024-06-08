c169c563746329d5fba4d0729d91d9d108310463
asher gabriela <109311931+ashcrysis@users.noreply.github.com>
2024-06-08 12:46:10 -0300

    Dialogue set up

---
+++ Assets/Scripts/DialoguePlayer.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialoguePlayer : MonoBehaviour
+{
+    [SerializeField] public GameObject dialogo;
+    private bool touching = false;
+    public bool resetLineIndex = false;
+    void Update()
+    {
+         if (GetComponentInChildren<Interavel>().canInteract){
+       if (Input.GetKeyDown(KeyCode.E))
+            {
+                 if (dialogo.GetComponent<dialogue>().index ==  dialogo.GetComponent<dialogue>().lines.Length-1){
+                      resetLineIndex = true;
+                    }
+                    dialogo.SetActive(true);
+                }
+        }
+    }
+
+
+  void OnCollisionEnter2D(Collision2D col)
+  {
+
+        if (col.gameObject.CompareTag("Player"))
+        {
+               touching = true;
+  }
+}
+
+  void OnCollisionExit2D(Collision2D col)
+  {
+
+        if (col.gameObject.CompareTag("Player"))
+        {
+               touching = false;
+        }
+
+}
+}
