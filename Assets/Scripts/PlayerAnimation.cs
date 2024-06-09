73e88eec8f8d3f1ee44738eb657a4cef1eef8525
asher gabriela <109311931+ashcrysis@users.noreply.github.com>
2024-06-09 15:50:01 -0300

    roll bugado p-p

--- Assets/Scripts/PlayerAnimation.cs
+++ Assets/Scripts/PlayerAnimation.cs
@@ -7,15 +7,18 @@ public class PlayerAnimation : MonoBehaviour
     private PlayerMovement playerMovement;
     private int moving;
     private Animator anim;
+    private Dash dash;
     void Start()
     {
         anim = GetComponentInChildren<Animator>();
         playerMovement = GetComponent<PlayerMovement>();
+        dash = GetComponent<Dash>();
     }
 
     void Update()
     {
         moving = playerMovement.moving;
         anim.SetInteger("moving", moving);
+        anim.SetBool("isDashing",dash.isDashing);
     }
 }
