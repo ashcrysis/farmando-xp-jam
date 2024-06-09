bf486ef0b2c470acc1db9f494cc5fadc9ec7987c
asher gabriela <109311931+ashcrysis@users.noreply.github.com>
2024-06-09 17:56:54 -0300

    Fade on teleporting

--- Assets/Scripts/Teleport.cs
+++ Assets/Scripts/Teleport.cs
@@ -1,21 +1,61 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Teleport : MonoBehaviour
 {
     public GameObject teleportPoint;
-    public bool trigger = false;
+    public Image fadeImage;
+    public float fadeDuration = 1.0f;
 
     void LateUpdate()
     {
-        if (GetComponent<Interavel>().canInteract && Input.GetKeyDown(KeyCode.E)){
-                GameObject.FindGameObjectWithTag("Player").gameObject.transform.position = teleportPoint.transform.position;
-        }
-
-        if (trigger){
-             GameObject.FindGameObjectWithTag("Player").gameObject.transform.position = teleportPoint.transform.position;
-             trigger = false;
+        if (GetComponent<Interavel>().canInteract && Input.GetKeyDown(KeyCode.E))
+        {
+            StartCoroutine(FadeAndTeleport());
         }
     }
+
+    IEnumerator FadeAndTeleport()
+    {
+        float elapsedTime = 0f;
+        Color color = fadeImage.color;
+
+        while (elapsedTime < fadeDuration)
+        {
+            elapsedTime += Time.deltaTime;
+            color.a = Mathf.Clamp01(elapsedTime / fadeDuration);
+            fadeImage.color = color;
+            yield return null;
+        }
+
+
+        color.a = 1f;
+        fadeImage.color = color;
+
+        //yield return new WaitForSeconds(2f);
+
+        GameObject.FindGameObjectWithTag("Player").transform.position = teleportPoint.transform.position;
+        yield return new WaitForSeconds(1f);
+        StartCoroutine(FadeImage(0f));
+    }
+      IEnumerator FadeImage(float targetAlpha)
+    {
+        float startAlpha = fadeImage.color.a;
+        float elapsedTime = 0f;
+
+        while (elapsedTime < fadeDuration)
+        {
+            elapsedTime += Time.deltaTime;
+            Color color = fadeImage.color;
+            color.a = Mathf.Lerp(startAlpha, targetAlpha, elapsedTime / fadeDuration);
+            fadeImage.color = color;
+            yield return null;
+        }
+
+        Color finalColor = fadeImage.color;
+        finalColor.a = targetAlpha;
+        fadeImage.color = finalColor;
+    }
+
 }
