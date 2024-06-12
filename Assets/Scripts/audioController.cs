e81a461d9c90112a450d8f4fe286fa7885be1095
asher gabriela <109311931+ashcrysis@users.noreply.github.com>
2024-06-12 12:16:33 -0300

    idle audio

--- Assets/Scripts/audioController.cs
+++ Assets/Scripts/audioController.cs
@@ -7,10 +7,12 @@ public class AudioController : MonoBehaviour
     public AudioSource deathSound;
     public AudioSource runAudio;
     public AudioSource walkAudio;
+    public AudioSource idleBreathingAudio;
     public bool onlyPlayDeathOnce = true;
-    
+
     private bool isWalkingAudioPlaying = false;
     private bool isRunningAudioPlaying = false;
+    private bool isIdleBreathingPlaying = false;
 
     void Update()
     {
@@ -31,7 +33,9 @@ public class AudioController : MonoBehaviour
             walkAudio.loop = true;
             isWalkingAudioPlaying = true;
             isRunningAudioPlaying = false;
+            isIdleBreathingPlaying = false;
             runAudio.Stop();
+            idleBreathingAudio.Stop();
         }
         else if (isMoving && isRunning && !isRunningAudioPlaying && playerMovement.IsGrounded())
         {
@@ -40,13 +44,31 @@ public class AudioController : MonoBehaviour
             runAudio.loop = true;
             isRunningAudioPlaying = true;
             isWalkingAudioPlaying = false;
+            isIdleBreathingPlaying = false;
             walkAudio.Stop();
+            idleBreathingAudio.Stop();
+        }
+        else if (!isMoving && !GetComponent<DeathCounter>().isDying && !isIdleBreathingPlaying && playerMovement.IsGrounded())
+        {
+            // Start idle breathing audio
+            idleBreathingAudio.Play();
+            idleBreathingAudio.loop = true;
+            isIdleBreathingPlaying = true;
+            isWalkingAudioPlaying = false;
+            isRunningAudioPlaying = false;
+            walkAudio.Stop();
+            runAudio.Stop();
         }
         else if (!isMoving || GetComponent<DeathCounter>().isDying)
         {
-            // Stop all audio when not moving
+            // Stop all audio when not moving or dying
             walkAudio.Stop();
             runAudio.Stop();
+            if (GetComponent<DeathCounter>().isDying)
+            {
+                idleBreathingAudio.Stop();
+                isIdleBreathingPlaying = false;
+            }
             isWalkingAudioPlaying = false;
             isRunningAudioPlaying = false;
         }
