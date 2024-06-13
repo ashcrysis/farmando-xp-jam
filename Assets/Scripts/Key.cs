07a93227917eff3fb06c6e0d498b87e22cd4195c
asher gabriela <109311931+ashcrysis@users.noreply.github.com>
2024-06-13 10:53:48 -0300

    Update Key.cs

--- Assets/Scripts/Key.cs
+++ Assets/Scripts/Key.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class Key : MonoBehaviour
 {
+    private bool hasCollectedKey = false;
+
     void Start()
     {
         if (PlayerPrefs.GetInt("hasKey") == 1)
@@ -11,13 +13,26 @@ public class Key : MonoBehaviour
             Destroy(gameObject);
         }
     }
+
     void Update()
     {
-        
-        if (Input.GetKeyDown(KeyCode.C) && GetComponent<Interavel>().canInteract && !GameObject.FindGameObjectWithTag("Player").GetComponent<DeathCounter>().isDying)
+        if (Input.GetKeyDown(KeyCode.C) && GetComponent<Interavel>().canInteract && !GameObject.FindGameObjectWithTag("Player").GetComponent<DeathCounter>().isDying && !hasCollectedKey)
         {
-            PlayerPrefs.SetInt("hasKey",1);
-            Destroy(gameObject);
-        }    
+            hasCollectedKey = true;
+            StartCoroutine(DelayedSetKey());
+        }
+    }
+
+    private IEnumerator DelayedSetKey()
+    {
+        yield return new WaitForSeconds(5);
+
+        // Check if the player is still alive after 5 seconds
+        if (!GameObject.FindGameObjectWithTag("Player").GetComponent<DeathCounter>().isDying)
+        {
+            PlayerPrefs.SetInt("hasKey", 1);
+        }
+
+        Destroy(gameObject);
     }
 }
