30dab9fe35633bbea9130636ea80ee28735ef4b4
asher gabriela <109311931+ashcrysis@users.noreply.github.com>
2024-06-14 08:00:33 -0300

    menu fade out

--- Assets/Scripts/MenuController.cs
+++ Assets/Scripts/MenuController.cs
@@ -1,16 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MenuController : MonoBehaviour
 {
     public GameObject creditsHolder;
     public GameObject buttonsHolder;        
+    public GameObject fadeImage;
+    public float fadeDuration = 1f; 
     public void NewGame()
     {
         PlayerPrefs.DeleteAll();
-        SceneManager.LoadScene(1);
+        StartCoroutine(FadeAndLoadScene(1));
     }
         public void Continue()
     {
@@ -19,17 +23,36 @@ public class MenuController : MonoBehaviour
             SceneManager.LoadScene(1);
         }
     }
-public void switchView()
-{
-    if (buttonsHolder.activeSelf)
+    public void switchView()
     {
-        buttonsHolder.SetActive(false);
-        creditsHolder.SetActive(true);
+        if (buttonsHolder.activeSelf)
+        {
+            buttonsHolder.SetActive(false);
+            creditsHolder.SetActive(true);
+        }
+        else if (creditsHolder.activeSelf)
+        {
+            creditsHolder.SetActive(false);
+            buttonsHolder.SetActive(true);
+        }
     }
-    else if (creditsHolder.activeSelf)
+   IEnumerator FadeAndLoadScene(int sceneIndex)
     {
-        creditsHolder.SetActive(false);
-        buttonsHolder.SetActive(true);
+        fadeImage.SetActive(true);
+        float elapsedTime = 0f;
+        Color color = fadeImage.GetComponent<Image>().color;
+
+        while (elapsedTime < fadeDuration)
+        {
+            elapsedTime += Time.deltaTime;
+            color.a = Mathf.Clamp01(elapsedTime / fadeDuration);
+            fadeImage.GetComponent<Image>().color = color;
+            yield return null;
+        }
+
+        color.a = 1f;
+        fadeImage.GetComponent<Image>().color = color;
+
+        SceneManager.LoadScene(sceneIndex);
     }
-}
 }
