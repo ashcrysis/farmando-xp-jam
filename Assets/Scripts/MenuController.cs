b007adbd8cbd660e389201e29da6d41bcc0bcf2e
asher gabriela <109311931+ashcrysis@users.noreply.github.com>
2024-06-14 11:38:10 -0300

    cutscene new game

--- Assets/Scripts/MenuController.cs
+++ Assets/Scripts/MenuController.cs
@@ -11,10 +11,33 @@ public class MenuController : MonoBehaviour
     public GameObject buttonsHolder;        
     public GameObject fadeImage;
     public float fadeDuration = 1f; 
+    public GameObject StartCutscene;
+    public bool animationOver = false;
+    private bool hasClicked = false;
+    private bool doOnce = false;
+    private bool doOnce2 = false;
     public void NewGame()
     {
+        hasClicked = true;
         PlayerPrefs.DeleteAll();
-        StartCoroutine(FadeAndLoadScene(1));
+        
+    }
+    void Update()
+    {
+        if (hasClicked && !doOnce)
+        {
+            StartCoroutine(FadeAndLoadScene(1));
+            doOnce = true;
+        }
+            if (animationOver && !doOnce2)
+            {
+                SceneManager.LoadScene(1);
+                doOnce2 = true;
+            }
+    }
+    public void animOver()
+    {
+        animationOver =  true;
     }
         public void Continue()
     {
@@ -52,7 +75,6 @@ public class MenuController : MonoBehaviour
 
         color.a = 1f;
         fadeImage.GetComponent<Image>().color = color;
-
-        SceneManager.LoadScene(sceneIndex);
+        StartCutscene.SetActive(true);
     }
 }
